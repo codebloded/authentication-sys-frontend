b3097ec862861c865490512f30afa2b83be6044d
codebloded <rohanrajput16.phy@gmail.com>
2020-09-12 05:39:46 +0530

    retriving data from server

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser')
-const auth = require("./verifyUser")
+const auth = require("./verifyUser");
+const { useState } = require("react");
+const User = require('../models/User');
+const mongoose = require("mongoose");
 
-router.get('/',auth, (req,res) => {
+
+router.get('/',auth,  (req,res) => {
+    
+    const idInfo =   User.findById({_id:req.user._id}).then(datax=>{
+        res.json(datax);
+        console.log(datax)
+    }).catch(err=>{
+        res.json(err);
+    })
+  
    
-    res.json(req.user)
-    console.log(req.user)
-   
+
 })
 
 module.exports = router;
