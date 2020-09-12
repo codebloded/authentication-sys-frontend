f02c2c676b529d877eeed2aa0824370001dd6ca5
codebloded <rohanrajput16.phy@gmail.com>
2020-09-12 10:16:53 +0530

    Final commit as of now :)

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import {Link , Button} from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
 
 function Logout(){
+    const [loggedIn, setLoggedIn] = useState(true);
+    const handler = () => {
+        setLoggedIn(false)
+        const removeToken = localStorage.removeItem('token');
+        if(removeToken===undefined){
+            console.log("logged out")
+        }
+       
 
+    } 
+    if(loggedIn === false){
+        return <Redirect to="/login"/>
+    }
 
-    
     return(
         <div className = "App">
             <h1>Logged out</h1>
+            <Link to="/logout" style={{textDecoration:"none", color:"white"}}><Button  onClick={handler} variant = "contained" color="primary">Logout</Button></Link>
+
             
         </div>
     )
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -2,32 +2,43 @@ import React,{useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import {Link, Redirect} from 'react-router-dom';
 import Axios from 'axios';
+import { Container } from '@material-ui/core';
 
 function User(){
-    const [apiData , setApiData] = useState();
-    useEffect(async ()=>{
+    const [apiData , setApiData] = useState({});
+    useEffect( ()=>{
         const Token = JSON.parse(localStorage.getItem('token'));
         console.log(Token);
-        const response = await Axios.get('http://localhost:4000/api/user',{
+        const response =  Axios.get('http://localhost:4000/api/user',{
+            
             headers:{
 
-                'Authorization': Token
+                'Authorization': Token,
+                'Content-Type': 'application/json',
+
             }
+        }).then(dataR=>{
+            console.log(dataR.data)
+            setApiData(dataR.data)
+        }).catch(err=>{
+            console.log(err);
         })
-        try {
-            console.log(response);
-        } catch (err) {
-            console.log(err.message)     
-        }
        
         
-    },)
+    },[setApiData])
     
     return(
         <div className='App'>
          
-        <h1>Sucessfully logged in </h1>
-        <h2>You are on the user page </h2>
+
+        <h1>Users</h1>
+        <Container>
+            <div className='App'>
+                <h3>Name {apiData.name}</h3>
+                <h3>Email {apiData.email}</h3>
+        
+            </div>
+        </Container>
         <Link to="/logout" style={{textDecoration:"none", color:"white"}}><Button variant = "contained" color="primary">Logout</Button></Link>
 
         </div>
