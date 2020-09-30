4c766f3a05b60550322f0b98404e943008aeaf47
Rohan kumar <48005711+codebloded@users.noreply.github.com>
2020-10-01 02:56:42 +0530

    Update verifyUser.js

--- server/routes/verifyUser.js
+++ server/routes/verifyUser.js
@@ -7,12 +7,12 @@ module.exports = function(req, res, next) {
         if(!token) return res.status(401).send("Access Denied");
         try{
             const userVerification = jwt.verify(token , process.env.TOKEN_SECRET);
-            console.log("token matched")
+            alert("token matched")
             req.user = userVerification;
             next();
 
         }catch(err){
-            console.log("Invalid Token")
+            alert("Invalid Token")
             res.status(400).send("Invalid Token");
         }
 }
